ff58ee937290252d61a1d949482eac44ab95f395
StanislavPetrovV <stvi4@outlook.com>
2022-06-24 13:22:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class Game(Ursina):
     def __init__(self):
         super().__init__()
         window.color = color.black
+        window.borderless = False
         # window.fullscreen_size = 1920, 1080
         # window.fullscreen = True
         Light(type='ambient', color=(0.5, 0.5, 0.5, 1))
